f38b2936b9781ed81f5fed146d3faee8e83e199c
Your Name <you@example.com>
2020-12-15 14:56:32 +0800

    add polygon

---
+++ src/label_instance.py
@@ -0,0 +1,371 @@
+# encoding:utf8
+
+import sys
+import cv2
+import numpy as np
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+from utils import getOnePolygon
+
+# f_handler=open('C:\qianlinjun\graduate\gen_dem\output\out.log', 'w')
+# sys.stdout=f_handler
+
+
+class Main(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.widget = ImageWithMouseControl(self)
+        self.widget.setGeometry(10, 10, 1200, 1200)
+        self.setWindowTitle('Image with mouse control')
+
+
+
+class PaintArea(QWidget):
+    PosChangeSignal = pyqtSignal(int,int)# 定义信号
+
+    def __init__(self):
+        super(PaintArea,self).__init__()
+        self.left_top_point = None
+        self.wait_paint_img     = None
+        self.setMinimumSize(800,800)
+
+        self.setMouseTracking(True)#get mouse in real time
+
+    def set_pos_and_img(self, left_top_point, img):
+        self.left_top_point = left_top_point
+        self.wait_paint_img     = img
+
+    def paintEvent(self, e):
+        '''
+        绘图
+        :param e:
+        :return:
+        '''
+        print("before paintEvent")
+        painter = QPainter()#self
+        painter.begin(self)
+        self.draw_img(painter)
+        painter.end()
+        print("after paintEvent")
+    
+    def draw_img(self, painter):
+        if self.left_top_point is None or self.wait_paint_img is None:
+            print("draw img args: left_top_point or scale img is none")
+            return
+
+        if type(self.wait_paint_img) == QPixmap:
+            print("before drawPixmap")
+            painter.drawPixmap(self.left_top_point, self.wait_paint_img)
+            print("after drawPixmap\n")
+        else:
+            print("before drawImage")
+            painter.drawImage(self.left_top_point, self.wait_paint_img)
+            print("after drawImage\n")
+        # self.left_top_point = None
+        # self.scaled_img     = None
+    
+    def mouseMoveEvent(self, e):
+        s = e.pos() #e.windowPos()
+        x = s.x()
+        y = s.y()
+        self.PosChangeSignal.emit(x , y)
+        
+
+
+def cvimg_to_qtimg(cvimg):
+
+    height, width, depth = cvimg.shape
+    cvimg = cv2.cvtColor(cvimg, cv2.COLOR_BGR2RGB)
+    cvimg = QImage(cvimg.data, width, height, width * depth, QImage.Format_RGB888)
+
+    return cvimg
+
+
+
+
+class ImageWithMouseControl(QWidget):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        print("before init")
+        self.parent = parent
+        self.qImg = None
+        self.scaled_img = None
+        self.scaled_img_mask = None
+        self.polygons = []# draw polygons in scaled mask
+
+        # value
+        self.floodFillthread = 5
+        # self.floodFillDowner = 5
+        
+        self.cv_img = None
+        self.left_top_point = QPoint(0,0) #只有在缩放和移动的时候才会改变
+        self.initUI()
+        print("after init")
+
+    def initUI(self):
+        # left paint area
+        spliter1 = QSplitter(Qt.Horizontal)
+        spliter1.setOpaqueResize(True)
+        frame1 = QFrame(spliter1)
+        layout_left = QVBoxLayout(frame1)
+        self.paintArea = PaintArea()
+        self.paintArea.PosChangeSignal.connect(self.posChangeCallback)
+        
+        layout_left.setSpacing(6)
+        layout_left.addWidget(self.paintArea)
+        
+
+        # right button area
+        self.loadBtn = QPushButton()
+        self.loadBtn.clicked.connect(self.loadFile)
+        self.loadBtn.setText("open")
+
+        self.spinbox = QSpinBox()
+        self.spinbox.setMinimum(1)
+        self.spinbox.setMaximum(254)
+        self.sld = QSlider(Qt.Horizontal, self)
+        self.sld.setMinimum(1)
+        self.sld.setMaximum(254)
+
+        self.spinbox.valueChanged.connect(lambda: self.sld.setValue(self.spinbox.value()))
+        self.sld.valueChanged.connect(self.changeFloodfillArgv)
+        self.sld.setValue(self.floodFillthread)
+
+        self.posInfoLabel = QLabel()
+        self.pixInfoLabel = QLabel()
+
+        spliter2 = QSplitter(Qt.Horizontal)
+        # spliter2.setOpaqueResize(True)
+        frame = QFrame(spliter2)
+        layoutRight = QGridLayout(frame)
+        layoutRight.addWidget(self.loadBtn, 1, 0)
+        layoutRight.addWidget(self.spinbox)
+        layoutRight.addWidget(self.sld)
+        layoutRight.addWidget(self.posInfoLabel)
+        layoutRight.addWidget(self.pixInfoLabel)
+        
+
+        fullLayout = QGridLayout(self)
+        fullLayout.addWidget(spliter1,0,0)
+        fullLayout.addWidget(spliter2,0,1)
+        self.setLayout(fullLayout)
+
+        # self.setWindowTitle('Image with mouse control')
+
+    def loadFile(self):
+        print("before load file")
+
+        print("before QFileDialog")
+        # load qpixmap
+        fname, ret = QFileDialog.getOpenFileName(self, '选择图片', 'c:\\', 'Image files(*.jpg *.gif *.png)')
+        if fname == "":
+            print("no valid image")
+            return
+        print("after QFileDialog")
+
+        self.qImg = QPixmap(fname)
+        self.scaled_img = self.qImg#.scaled(self.qImg.size())
+        # resize paint area
+        self.paintArea.setMinimumSize(self.qImg.width(), self.qImg.height())
+        
+        # cv image to flood fill
+        self.cv_img = cv2.imread(fname)
+        gray2 = cv2.cvtColor(self.cv_img, cv2.COLOR_BGR2RGB)
+        self.gray  = cv2.cvtColor(gray2, cv2.COLOR_RGB2GRAY)
+        self.ori_img_wh = self.cv_img.shape
+
+        self.paintArea.set_pos_and_img(self.left_top_point, self.scaled_img)
+        self.repaint()
+
+        print("after load file")
+    
+    def changeFloodfillArgv(self, v):
+        self.spinbox.setValue(v)
+        self.floodFillthread = v
+        print("self.floodFillthread :", v)
+
+    def mouseMoveEvent(self, e):  # 重写移动事件
+        print("before movement")
+        if self.qImg is None or self.scaled_img is None:
+            print("img is None or or scaled_img is None  in mouseMoveEvent ")
+            return
+
+        if self.left_click:
+            self._endPos = e.pos() - self._startPos
+            self.left_top_point = self.left_top_point + self._endPos
+            self._startPos = e.pos()
+            self.paintArea.set_pos_and_img(self.left_top_point, self.scaled_img)
+            self.repaint()
+        
+        print("after movement")
+
+
+
+    def mousePressEvent(self, e):
+        '''
+        点击产生漫水填充算法
+        '''
+
+        print("before mousePressEvent")
+        if self.qImg is None or self.scaled_img is None:
+            print("img is None or or scaled_img is None  in mousePressEvent ")
+            return
+
+        if e.button() == Qt.LeftButton:
+            self.left_click = True
+            self._startPos = e.pos()
+            self.floodFill()
+  
+        print("after mousePressEvent")
+    
+
+    def floodFill(self):
+        '''
+        get segmentation by  mouse click
+        '''
+        flag, ptLocxy = self.getPtMapInImg(QPoint(self.mousePos.x(), self.mousePos.y()))
+        if flag is True:
+            # fill_color_demo(self.gray, seedPt)
+
+            copyImg = self.gray.copy()
+            h, w = self.gray.shape[:2]
+            mask = np.zeros([h+2, w+2], np.uint8)
+
+            
+            mask_fill = 252 #mask的填充值
+            #floodFill充值标志
+            flags = 4|(mask_fill<<8)|cv2.FLOODFILL_FIXED_RANGE
+
+            # mask 需要填充的位置设置为0
+            cv2.floodFill(copyImg, mask, ptLocxy, 255, self.floodFillthread, self.floodFillthread, flags)  #(836, 459) , cv2.FLOODFILL_FIXED_RANGE
+            # cv2.imwrite("C:\qianlinjun\graduate\gen_dem\output\mask.png", mask)
+            # cv2.waitKey(1000)
+
+            alpha = 0.7
+            beta = 1-alpha
+            gamma = 0
+
+            # mask = np.vstack( (copyImg[:,:,np.newaxis], np.zeros([h, w, 2], np.uint8) ))
+            mask = cv2.resize(mask, (h, w))
+            mask_rgb = np.concatenate((np.ones([h, w, 2], dtype=np.uint8),  mask[:,:,np.newaxis].astype(np.uint8) ), axis=-1)
+            mask_rgb[mask == 0] = [127,127,127]
+            mask_rgb[mask == 255] = [255,0,0]
+            self.scaled_img_mask = mask_rgb
+
+            polygon = getOnePolygon(self.scaled_img_mask)
+
+            
+            img_add       = cv2.addWeighted(self.cv_img, alpha, mask_rgb, beta, gamma)# add mask
+            qt_add_img    = cvimg_to_qtimg(img_add)
+            scaled_qimg   = qt_add_img.scaled(self.scaled_img.size())
+            scaled_Pixmap = QPixmap.fromImage(scaled_qimg)
+            
+            self.paintArea.set_pos_and_img(self.left_top_point, scaled_Pixmap)
+            self.repaint()
+
+
+    def mouseReleaseEvent(self, e):
+        print("before mouseReleaseEvent")
+        if self.qImg is None or self.scaled_img is None:
+            print("img is None or or scaled_img is None  in mouseReleaseEvent ")
+            return
+
+        if e.button() == Qt.LeftButton:
+            self.left_click = False
+        # elif e.button() == Qt.RightButton:
+        #     self.left_top_point = QPoint(0, 0)
+        #     self.scaled_img = self.qImg.scaled(self.size())
+        #     self.paintArea.set_pos_and_img(self.left_top_point, self.scaled_img)
+        #     self.repaint()
+        print("after mouseReleaseEvent")
+
+
+    def wheelEvent(self, e):
+        '''
+        spin wheel to zoom in/out image
+        '''
+        print("before wheelEvent")
+
+        if self.qImg is None or self.scaled_img is None:
+            print("img is None or or scaled_img is None  in wheelEvent ")
+            return
+
+        if e.angleDelta().y() < 0:
+            # 放大图片
+            self.scaled_img = self.qImg.scaled(self.scaled_img.width()-15, self.scaled_img.height()-15)
+            new_w = e.x() - (self.scaled_img.width() * (e.x() - self.left_top_point.x())) / (self.scaled_img.width() + 5)
+            new_h = e.y() - (self.scaled_img.height() * (e.y() - self.left_top_point.y())) / (self.scaled_img.height() + 5)
+            # print(new_w, new_h)
+            self.left_top_point = QPoint(new_w, new_h)
+
+        elif e.angleDelta().y() > 0:
+            # 缩小图片
+            self.scaled_img = self.qImg.scaled(self.scaled_img.width()+15, self.scaled_img.height()+15)
+            new_w = e.x() - (self.scaled_img.width() * (e.x() - self.left_top_point.x())) / (self.scaled_img.width() - 5)
+            new_h = e.y() - (self.scaled_img.height() * (e.y() - self.left_top_point.y())) / (self.scaled_img.height() - 5)
+            self.left_top_point = QPoint(new_w, new_h)
+        
+        self.paintArea.set_pos_and_img(self.left_top_point, self.scaled_img)
+        self.repaint()
+        print("after wheelEvent")
+
+    # def resizeEvent(self, e):
+    #     if self.parent is not None and self.qImg is not None:
+    #         self.scaled_img = self.qImg.scaled(self.size())
+    #         self.left_top_point = QPoint(0, 0)
+    #         self.update()
+
+    def  posChangeCallback(self, x, y):
+        '''
+        receive mouse pos change signal 
+        '''
+        print("before posChangeCallback")
+        text = "x: {0},  y: {1}".format(x, y)
+        self.mousePos = QPoint(x, y)
+        self.posInfoLabel.setText(text)
+
+        if self.cv_img is not None:
+            flag, ptLocxy = self.getPtMapInImg(QPoint(x, y))
+            if flag is True:
+                pixel_value = self.cv_img[ptLocxy[1], ptLocxy[0]]
+                text = "{0} {1} {2}".format(pixel_value[2], pixel_value[1], pixel_value[0])
+                self.pixInfoLabel.setText(text)
+        print("after posChangeCallback")
+    
+    def getPtMapInImg(self, curPos):
+        '''
+        get mouse's point location mapped in image
+        '''
+        print("before getPtMapInImg")
+        # check valid window
+        if curPos.x() <= 0 or curPos.x() >= self.paintArea.width() or curPos.y() <= 0 or curPos.y() >= self.paintArea.height():
+            print("after getPtMapInImg False, (-1, -1)")
+            return False, (-1, -1)
+
+        if self.scaled_img is not None:
+            relat_pos = curPos - self.left_top_point
+            
+            if relat_pos.x() > 0 and relat_pos.x() < self.scaled_img.width() and relat_pos.y() > 0 and relat_pos.y() < self.scaled_img.height():
+                # 得到鼠标点击位置在原始图片上的位置 作为漫水填充算法种子点
+                point_map2_img_x = int(relat_pos.x()*1.0 / self.scaled_img.width() *  self.ori_img_wh[0])
+                point_map2_img_y = int(relat_pos.y()*1.0 / self.scaled_img.height() *  self.ori_img_wh[1] )
+                print(curPos, (self.left_top_point.x(), self.left_top_point.y()), (point_map2_img_x, point_map2_img_y))
+                print("after getPtMapInImg")
+                return True, (point_map2_img_x, point_map2_img_y)
+            else:
+                print("after getPtMapInImg relat_pos.x() > 0 is false")
+            
+        return False, (-1, -1)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Main()
+    # ex = ImageWithMouseControl()
+    ex.show()
+    app.exec_()
---
+++ src/utils.py
@@ -0,0 +1,248 @@
+import sys
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+class Polygon(object):
+    object_id = 0
+    def __init__(self, polygon):
+        self.id = Polygon.object_id
+        Polygon.object_id += 1
+        self.polygon = polygon
+        self.area = cv2.contourArea(polygon)
+        self.mountain_line = None
+    
+    def set(self, polygon):
+        self.polygon = polygon
+    
+    def get(self):
+        return self.polygon
+    
+    def getArea(self):
+        return self.area
+
+
+# img1 = cv2.imread(sys.argv[1])
+# gray2 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+# gray  = cv2.cvtColor(gray2, cv2.COLOR_RGB2GRAY)
+
+
+# plt.imshow(gray),
+# plt.title('Original')
+# plt.show()
+
+# # Otsu ' s 二值化；
+# ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY +cv2.THRESH_OTSU)#_INV
+# print("ret", ret)
+
+# plt.subplot(1,3,1),
+# plt.imshow(gray2),
+# plt.title('Original')
+# plt.subplot(1,3,3),
+# plt.imshow(thresh),
+# plt.title("thresh")
+# plt.show()
+
+# #nosing removoal迭代两次
+# kernel = np.ones((3,3),np.uint8)
+# opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations = 2)
+ 
+# # sure background area
+# sure_bg = cv2.dilate(opening, kernel,iterations = 3)
+# plt.imshow(opening),
+# plt.title('opening')
+# plt.imshow(sure_bg),
+# plt.title('sure_bg')
+# plt.show()
+
+# dist_transform = cv2.distanceTransform(opening,1,5)
+# ret, sure_fg = cv2.threshold(dist_transform, 0.7*dist_transform.max(), 255,0)
+ 
+# sure_fg = np.uint8(sure_fg)
+# unknow = cv2.subtract(sure_bg,sure_fg)
+ 
+ 
+# plt.subplot(1,3,1),
+# plt.imshow(sure_bg),
+# plt.title('Black region\n must be background')
+ 
+# plt.subplot(1,3,3),
+# plt.imshow(unknow),
+# plt.title('Yellos region\n must be foregroun')
+
+
+# #Marker labeling
+# ret, makers1 = cv2.connectedComponents(sure_fg)
+ 
+# #Add one to all labels so that sure background is not 0 but 1;
+# markers = makers1 +1
+ 
+# #Now mark the region of unknow with zero;
+# markers[unknow ==255] =0
+# markers3 = cv2.watershed(img1, markers)
+
+
+# img1[markers3 == -1] =[255,0,0]
+ 
+# plt.subplot(1,3,1),
+# plt.imshow(makers1),
+# plt.title('makers1')
+# plt.subplot(1,3,2),
+# plt.imshow(markers3),
+# plt.title('markers3')
+# plt.subplot(1,3,3),
+# plt.imshow(img1),
+# plt.title('img1')
+
+# plt.show()
+
+
+
+def fill_color_demo(img_path, pos=(1,1)):
+    image = cv2.imread(img_path)
+    gray2 = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    gray  = cv2.cvtColor(gray2, cv2.COLOR_RGB2GRAY)
+    copyIma = image.copy()
+    h, w = image.shape[:2]
+    mask = np.zeros([h+2, w+2], np.uint8)
+    mask_fill = 252 #mask的填充值
+    #floodFill充值标志
+    flags = 4|(mask_fill<<8)#|cv2.FLOODFILL_FIXED_RANGE
+    
+    # mask 需要填充的位置设置为0
+    cv2.floodFill(copyIma, mask, pos, 255, 5, 5 , flags)  #(836, 459) , cv2.FLOODFILL_FIXED_RANGE
+    # cv.imshow("fill_color", copyIma)
+    plt.subplot(1,2,1)
+    plt.imshow(image),
+    plt.title('image')
+    plt.subplot(1,2,2)
+    plt.imshow(copyIma),
+    plt.title('copyIma')
+
+    plt.show()
+    # return mask
+
+
+
+def getOnePolygon(img):
+    # img = cv2.imread('002.tif')
+    rows, cols, ch = img.shape
+    # 边缘提取
+    Ksize = 3
+    L2g = True
+    edge = cv2.Canny(img, 50, 100, apertureSize=Ksize, L2gradient=L2g)
+
+    # 提取轮廓
+    '''
+    findcontour()函数中有三个参数，第一个img是源图像，第二个model是轮廓检索模式，第三个method是轮廓逼近方法。输出等高线contours和层次结构hierarchy。
+    model:  cv2.RETR_EXTERNAL  仅检索极端的外部轮廓。 为所有轮廓设置了层次hierarchy[i][2] = hierarchy[i][3]=-1
+            cv2.RETR_LIST  在不建立任何层次关系的情况下检索所有轮廓。
+            cv2.RETR_CCOMP  检索所有轮廓并将其组织为两级层次结构。在顶层，组件具有外部边界；在第二层，有孔的边界。如果所连接零部件的孔内还有其他轮廓，则该轮廓仍将放置在顶层。
+            cv2.RETR_TREE  检索所有轮廓，并重建嵌套轮廓的完整层次。
+            cv2.RETR_FLOODFILL  输入图像也可以是32位的整型图像(CV_32SC1)
+    method：cv2.CHAIN_APPROX_NONE  存储所有的轮廓点，任何一个包含一两个点的子序列（不改变顺序索引的连续的）相邻。
+            cv2.CHAIN_APPROX_SIMPLE  压缩水平，垂直和对角线段，仅保留其端点。 例如，一个直立的矩形轮廓编码有4个点。
+            cv2.CHAIN_APPROX_TC89_L1 和 cv2.CHAIN_APPROX_TC89_KCOS 近似算法
+    '''
+    contours, hierarchy = cv2.findContours(edge, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # 绘制轮廓 第三个参数是轮廓的索引（在绘制单个轮廓时有用。要绘制所有轮廓，请传递-1）
+    print("contours", len(contours))
+
+    if len(contours) == 0:
+      return False, None
+
+    min_area = 10000000000000000000000000
+    min_idx = 0
+    #find min area
+    for idx, cnt in enumerate(contours):
+        area = cv2.contourArea(cnt)
+        print("area", area)
+        if area > 10 and area < min_area:
+            min_idx = idx
+            min_area = area
+    
+    cnt = contours[min_idx]
+
+    # dst = np.ones(img.shape, dtype=np.uint8)
+    # # cv2.drawContours(dst, contours, -1, (0, 255, 0), 1)
+    # # 绘制单个轮廓
+    # print("min_idx", min_idx)
+    # cnt = contours[min_idx]
+    # cv2.drawContours(dst, [cnt], 0, (0, 0, 255), 1)
+
+    # # 轮廓面积
+    # area = cv2.contourArea(cnt)
+    # print("minarea", area)
+
+    # # 特征矩
+    # M = cv2.moments(cnt)
+    # # print(M)
+    # cx = int(M['m10']/M['m00'])
+    # cy = int(M['m01']/M['m00'])
+    # cv2.circle(dst, (cx, cy), 2, (0, 0, 255), -1)   # 绘制圆点
+
+
+
+    # 轮廓周长：第二个参数指定形状是闭合轮廓(True)还是曲线
+    # perimeter = cv2.arcLength(cnt, True)
+    # print(perimeter)
+
+    # 轮廓近似：epsilon是从轮廓到近似轮廓的最大距离--精度参数
+    # epsilon = 0.01 * cv2.arcLength(cnt, True)
+    # approx = cv2.approxPolyDP(cnt, epsilon, True)
+    # cv2.polylines(dst, [approx], True, (0, 255, 255))   # 绘制多边形
+    # print(approx)
+
+    # 轮廓凸包：returnPoints：默认情况下为True。然后返回凸包的坐标。如果为False，则返回与凸包点相对应的轮廓点的索引。
+    # hull = cv2.convexHull(cnt, returnPoints=True)
+    # cv2.polylines(dst, [hull], True, (255, 255, 255), 2)   # 绘制多边形
+    # print(hull)
+
+    # 检查凸度：检查曲线是否凸出的功能，返回True还是False。
+    # k = cv2.isContourConvex(cnt)
+    # print(k)
+
+    # 直角矩形
+    # x, y, w, h = cv2.boundingRect(cnt)
+    # cv2.rectangle(dst, (x, y), (x+w, y+h), (255, 255, 0), 2)
+    
+    # 旋转矩形
+    # rect = cv2.minAreaRect(cnt)
+    # box = cv2.boxPoints(rect)
+    # box = np.int0(box)
+    # cv2.drawContours(dst, [box], 0, (0, 0, 255), 2)
+
+    # 最小外接圆
+    # (x, y), radius = cv2.minEnclosingCircle(cnt)
+    # center = (int(x), int(y))
+    # radius = int(radius)
+    # cv2.circle(dst, center, radius, (0, 255, 0), 2)
+
+    # 拟合椭圆
+    # ellipse = cv2.fitEllipse(cnt)
+    # cv2.ellipse(dst, ellipse, (0, 0, 255), 2)
+
+    # 拟合直线
+    # rows, cols = img.shape[:2]
+    # [vx, vy, x, y] = cv2.fitLine(cnt, cv2.DIST_L2, 0, 0.01, 0.01)
+    # lefty = int((-x*vy/vx) + y)
+    # righty = int(((cols-x)*vy/vx)+y)
+    # cv2.line(dst, (cols-1, righty), (0, lefty), (255, 255, 255), 2)
+
+
+    # cv2.imshow("dst", dst)
+    # cv2.waitKey()
+    return Polygon(cnt)
+
+
+
+
+
+
+
+
+# if __name__ == "__main__":
+#   fill_color_demo(sys.argv[1], (811,490))
+
+

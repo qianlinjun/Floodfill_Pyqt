4d5fdba1c3f995ca2086225c9a37634c458d509d
Your Name <you@example.com>
2021-03-14 18:58:13 +0800

    add node substitute cost

--- src/graph.py
+++ src/graph.py
@@ -1,11 +1,21 @@
 import os
+import sys
+import cv2
 import math
 import json
+import numpy as np
 import networkx as nx
 # import gmatch4py as gm
 import matplotlib.pyplot as plt 
 
+from poly_util import get_iou
+
 img_w , img_h = 1024, 1024
+
+
+fsock = open('C:\qianlinjun\graduate\gen_dem\output\src\graph_match_out.txt', 'a+')
+sys.stdout = fsock
+print("\n\n-----------------------------------------\n")
 
 def compute_angle(G_node1, G_node2):
     '''
@@ -74,7 +84,7 @@ def buildGraphFromJson(json_file):
     # node edit cost
     # 山体 水 平原 森林 房屋
     # 同一个类别计算相似度
-    scene_graph = nx.Graph() # 建立一个空的无向图G
+    scene_graph = nx.Graph(name=json_file) # 建立一个空的无向图G
     
     polygons = json.load(open(json_file,'r'))
 
@@ -104,7 +114,7 @@ def buildGraphFromJson(json_file):
             scene_graph.add_edge(id_, id_1, name='{} - {}'.format(id_, id_1), angle=angle, dist=dist)
     
     # [2] add global nodes
-    print("len poly:{}".format(len(polygons)))
+    # print("len poly:{}".format(len(polygons)))
     global_id      = "global" 
     global_contour = [[[0,0]],[[0,1024]],[[1024,1024]],[[1024,0]]]
     global_moment  = [total_moment_x/len(polygons),total_moment_y/len(polygons)]
@@ -163,14 +173,19 @@ def search_graph(query_G, scene_graphs):
     def node_subst_cost(node1, node2):
         node1 = node1["scene_node"]
         node2 = node2["scene_node"]
-        # print(node1)
+        # print(node1.id)
+
         #global 
         if node1.nodeType   == "global" and node2.nodeType == "global":
             # 位置距离
             # 整体的山脊线 差距
-            return node1.compute_dist(node2)
+            cost = node1.compute_dist(node2)
+            print("global global dist:{}".format(cost))
+            # return dist
         elif node1.nodeType == "global" or  node2.nodeType == "global":
-            return 10000000000000
+            # return 10000000000000
+            cost = 10000000000000
+            print("global node dist:{}".format(cost))
         else:
             # 普通节点  
             # 类别距离
@@ -182,44 +197,62 @@ def search_graph(query_G, scene_graphs):
             # dist_cost = node1.compute_dist(node2)
 
             # area 像素误差
-            area_cost = abs(node1.area  - node2.area)
+            # area_cost = abs(node1.area  - node2.area)
             
-            print("abs_area: {}".format(area_cost))
+            # hu 矩具有
+            poly1 = node1.contour
+            poly2 = node2.contour
+            poly1 = np.array(poly1).squeeze()
+            poly2 = np.array(poly2).squeeze()
+            poly1_area, poly2_area, iou = get_iou(poly1, poly2)
+            shape_cost = cv2.matchShapes(poly1, poly2,  2, 0.0) *  1. / (iou + 0.1) * max(poly1_area, poly2_area) / min(poly1_area, poly2_area)
+            print("node:{} node:{} shape_cost: {}".format(node1.id, node2.id, shape_cost))
 
             #shaply
 
             #山脊线特征距离
             skyline_cost = 0
 
-            return class_cost + area_cost + skyline_cost
+            cost = class_cost + shape_cost + skyline_cost
+        
+        return cost
     
     def node_del_cost(node1):
-        return 10000
+        # 当前节点和所有对方节点的替换损失最小值*0.9
+        
+        return math.sqrt(node1.area)
+
+
 
     def node_ins_cost(node2):
-        return 10000
+
+        return math.sqrt(node2.area)
     
 
     def edge_subst_cost(edge1, edge2):
-        abs_angle = abs(edge1["angle"] - edge2["angle"])/3.1415926 * 180 # 3.14
-        abs_dist  = abs(edge1["dist"] - edge2["dist"])
-        print("edge cost:{}".format(abs_angle + abs_dist))
-        return abs_angle + abs_dist
+        # abs_angle = abs(edge1["angle"] - edge2["angle"])/3.1415926 * 180 # 3.14
+        # abs_dist  = abs(edge1["dist"] - edge2["dist"])
+        # print("edge cost:{}".format(abs_angle + abs_dist))
+        return 0 #abs_angle + abs_dist
     
 
     def edge_del_cost(node1):
-        return 10000
+        return 0
     def edge_ins_cost(node1):
-        return 10000
-
-
-
+        return 0
 
 
     minimum_cost = 100000000000
     result_G = None
+    best_vertex_path = None 
+    best_edge_path   = None
+    
     for scene_G in scene_graphs:
-        ge_dist = nx.graph_edit_distance(query_G, scene_G, 
+        # 自定义替换损失
+        # 自定义插入 删除损失
+        
+        print("\n calculate dist between {} {}".format(query_G.name, scene_G.name))
+        ge_dist, vertex_path, edge_path = nx.graph_edit_distance(query_G, scene_G, 
                                         node_subst_cost = node_subst_cost, 
                                         node_del_cost = node_del_cost,
                                         node_ins_cost = node_ins_cost,
@@ -227,14 +260,16 @@ def search_graph(query_G, scene_graphs):
                                         edge_del_cost = edge_del_cost,
                                         edge_ins_cost = edge_ins_cost)
 
-        print("dist:{}".format(ge_dist))
+        # print("dist:{}".format(ge_dist))
         if minimum_cost > ge_dist:
             minimum_cost = ge_dist
             result_G     = scene_G
+            best_vertex_path = vertex_path
+            best_edge_path   = edge_path
     
     DrawGraph(result_G)
     
-    print("search result:{}".format(minimum_cost))
+    print("search result:{} cost:{} vertex path:{} edge path:{} \n".format(result_G.name, minimum_cost, best_vertex_path, edge_path))
 
 
 if __name__ == "__main__":
@@ -243,8 +278,12 @@ if __name__ == "__main__":
     search_pic_w = 800
     search_pic_h = 800
     # save_path = "C:\qianlinjun\graduate\gen_dem\output\img"
+
     save_path = "C:\qianlinjun\graduate\gen_dem\output\img_with_mask\switz-100-points"
     scene_graphs = loadGraphsFromJson(save_path, visualize=False) #"8.59262657_46.899601"
-    print(len(scene_graphs))
+    
     if len(scene_graphs) > 2:
         search_graph(scene_graphs[0], scene_graphs[1:])
+
+    print("total: {} search: {} \n\n".format(len(scene_graphs), scene_graphs[0].name))
+    fsock.close()
